7867fa0b3f14e98d1ee46777a0742c955104656b
CARLOS LEONARDO MARIANO <carloslmariano2000@gmail.com>
2024-10-24 20:35:50 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const url = document.getElementById('url')
+const botao = document.getElementById('btn-url')
+
+
+botao.addEventListener('click',(evt)=>{
+window.location.assign('https://www.xvideos.com/home/br')
+})
